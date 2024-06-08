5d5b44d516342f9280bb6ab62863f53af2437f06
Alvarisha WL <118552236+acha-willbertha@users.noreply.github.com>
2024-06-08 04:42:39 +0000

    Update add-ssws.sh

--- ssws/add-ssws.sh
+++ ssws/add-ssws.sh
@@ -4,25 +4,6 @@ red() { echo -e "\\033[32;1m${*}\\033[0m"; }
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-CEKEXPIRED () {
-    today=$(date -d +1day +%Y-%m-%d)
-    Exp1=$(curl -sS https://raw.githubusercontent.com/andristji/Xray-SSH/main/permission/ip | grep $MYIP | awk '{print $3}')
-    if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mSTATUS SCRIPT AKTIF...\e[0m"
-    else
-    echo -e "\e[31mSCRIPT ANDA EXPIRED!\e[0m";
-    exit 0
-fi
-}
-IZIN=$(curl -sS https://raw.githubusercontent.com/andristji/Xray-SSH/main/permission/ip | awk '{print $4}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-CEKEXPIRED
-else
-echo -e "\e[31mPermission Denied!\e[0m";
-exit 0
-fi
-clear
 
 domain=$(cat /etc/xray/domain)
 clear
