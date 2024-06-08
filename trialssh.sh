62a5f598bb201c2d277ba19e075d0fb774bf3e07
Alvarisha WL <118552236+acha-willbertha@users.noreply.github.com>
2024-06-08 10:50:34 +0000

    Update trialssh.sh

--- trialssh.sh
+++ trialssh.sh
@@ -25,24 +25,6 @@ export ungu='\033[0;35m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "memeriksa vps anda"
 sleep 0.5
-CEKEXPIRED () {
-        today=$(date -d +1day +%Y -%m -%d)
-        Exp1=$(curl -sS https://raw.githubusercontent.com/AndyyudaVPN/permission/main/ip | grep $MYIP | awk '{print $3}')
-        if [[ $today < $Exp1 ]]; then
-        echo "status script aktif.."
-        else
-        echo "SCRIPT ANDA EXPIRED";
-        exit 0
-fi
-}
-IZIN=$(curl -sS https://raw.githubusercontent.com/AndyyudaVPN/permission/main/ip | awk '{print $4}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo "IZIN DI TERIMA!!"
-CEKEXPIRED
-else
-echo "Akses di tolak!! Benget sia hurung!!";
-exit 0
-fi
 
 # Getting
 Login=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
