a979ba97c1b4c550816f7f9c34f98edfb1f920e0
Alvarisha WL <118552236+acha-willbertha@users.noreply.github.com>
2024-06-08 11:01:38 +0000

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -17,10 +17,10 @@ locality=none
 organization=none
 organizationalunit=TMSC
 commonname=none
-email=agunxz@themoon.my,id
+email=88numb@gmail.com
 
 # simple password minimal
-wget -O /etc/pam.d/common-password "https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/password.txt"
+wget -O /etc/pam.d/common-password "https://raw.githubusercontent.com/acha-willbertha/jang-ganggu/main/password.txt"
 chmod +x /etc/pam.d/common-password
 
 # go to root
@@ -129,20 +129,20 @@ apt -y install nginx
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/acha-willbertha/jang-ganggu/main/nginx.conf"
 mkdir -p /home/vps/public_html
 /etc/init.d/nginx restart
 
 # install badvpn
 cd
 cd
-wget -O /usr/bin/badvpn-udpgw https://github.com/AndyyudaVPN/xray-ws/main/badvpn/badvpn-udpgw && chmod +x  /usr/bin/badvpn-udpgw
+wget -O /usr/bin/badvpn-udpgw https://github.com/acha-willbertha/jang-ganggus/main/badvpn/badvpn-udpgw && chmod +x  /usr/bin/badvpn-udpgw
 #system badvpn 7300
-wget -O /etc/systemd/system/svr-7300.service https://github.com/AndyyudaVPN/xray-ws/main/badvpn/svr-7300.service && chmod +x  /etc/systemd/system/svr-7300.service
+wget -O /etc/systemd/system/svr-7300.service https://github.com/acha-willbertha/jang-ganggu/main/badvpn/svr-7300.service && chmod +x  /etc/systemd/system/svr-7300.service
 #system badvpn 7200
-wget -O /etc/systemd/system/svr-7200.service https://github.com/AndyyudaVPN/xray-ws/main/badvpn/svr-7200.service && chmod +x  /etc/systemd/system/svr-7200.service
+wget -O /etc/systemd/system/svr-7200.service https://github.com/acha-willbertha/jang-ganggu/main/badvpn/svr-7200.service && chmod +x  /etc/systemd/system/svr-7200.service
 #system badvpn 7100
-wget -O /etc/systemd/system/svr-7100.service https://github.com/AndyyudaVPN/xray-ws/main/badvpn/svr-7100.service && chmod +x  /etc/systemd/system/svr-7100.service
+wget -O /etc/systemd/system/svr-7100.service https://github.com/acha-willbertha/jang-ganggu/main/badvpn/svr-7100.service && chmod +x  /etc/systemd/system/svr-7100.service
 
 #reboot system 7100
 systemctl daemon-reload
